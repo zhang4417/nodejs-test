91b9700918fa43af9d8299d5de091d9710dd1ac7
zhang4417 <56223092+zhang4417@users.noreply.github.com>
2019-10-25 17:20:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -24,19 +24,95 @@ var server = http.createServer(function (request, response) {
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/html;charset=utf-8');
         response.write(`
-            <!DOCTYPE html>
-            <head>
-                <link rel="stylesheet" href="/x">
-            </head>
-            <body>
-                <h1>哈哈哈</h1>
-            </body>
+         <!DOCTYPE html>
+         <html lang="zh-CN">
+
+         <head>
+         <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+         <title>画线</title>
+            <link rel="stylesheet" href="/x" >
+            <script src="/y"></script>
+        </head>
+
+        <body>
+            <canvas id="canvas"></canvas>
+        </body>
+        </html>
         `);
         response.end();
     } else if (path === '/x') {
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/css;charset=utf-8');
-        response.write(`body{color:red;}`);
+        response.write(`
+          *{padding:0;margin:0;box-sizing:border-box;}
+          #canvas{display:block;}
+          `);
+        response.end();
+    } else if (path === '/y') {
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/javascript;charset=utf-8');
+        response.write(`
+                    let canvas = document.getElementById('canvas');
+        canvas.width = document.documentElement.clientWidth;
+        canvas.height = document.documentElement.clientHeight;
+        let ctx = canvas.getContext('2d');
+        ctx.fillStyle = "green";
+        ctx.strokeStyle = "green"
+        ctx.lineCap = "round"
+        ctx.lineWidth = 4
+
+        function drawLine(x1, y1, x2, y2) {
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.stroke();
+        }
+
+        let painting = false;
+        let last
+
+
+        let isTouchDevice = 'ontouchstart' in document.documentElement;
+        if (isTouchDevice) {
+            canvas.ontouchstart = function (e) {
+                let x = e.touches[0].clientX;
+                let y = e.touches[0].clientY;
+                last = [x, y]
+            }
+            canvas.ontouchmove = function (e) {
+                // let x = e.touches[0].clientX;
+                // let y = e.touches[0].clientY;
+                // ctx.beginPath();
+                // ctx.arc(x, y, 10, 0, 2 * Math.PI);
+                // ctx.fill();
+
+                let x = e.touches[0].clientX;
+                let y = e.touches[0].clientY;
+                drawLine(last[0], last[1], x, y);
+                last = [x, y]
+            }
+        } else {
+            canvas.onmousedown = function (e) {
+                painting = true;
+                last = [e.clientX, e.clientY]
+            }
+            canvas.onmousemove = function (e) {
+                if (painting === true) {
+                    // ctx.beginPath();
+                    // ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI);
+                    // ctx.fill();
+
+                    drawLine(last[0], last[1], e.clientX, e.clientY)
+                    last = [e.clientX, e.clientY]
+                }
+            }
+            canvas.onmouseup = function () {
+                painting = false;
+            }
+        }
+            `);
         response.end();
     } else {
         response.statusCode = 404
